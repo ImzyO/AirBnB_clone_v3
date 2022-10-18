362790fa3c01a9096167b686e2b3f581618c408a
Imani <100032916+ImzyO@users.noreply.github.com>
2022-10-18 17:07:14 +0300

    full deploy

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+"""distributes an archive to your web servers, using the function do_deploy:"""
+from os.path import exists
+from datetime import datetime
+from fabric.api import local, env, put, run
+
+env.hosts = ['3.229.120.108', '44.192.24.108']
+
+def do_pack():
+    """Function compresses file"""
+    date_time = datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")
+    path = "versions/web_static_{}.tgz".format(date_time)
+    local("mkdir -p versions")
+    result = local("tar -cvzf {} web_static".format(path))
+    if result.failed:
+        return None
+    return path
+
+
+def do_deploy(archive_path):
+    """Function to distributes archive to server"""
+    if exists(archive_path) is False:
+        return False
+    file_name = archive_path.split("/")[-1]
+    file_noext = file_name.split(".")[0]
+    path = "/data/web_static/releases/"
+    res = put(archive_path, '/tmp/')
+    if res.failed:
+        return False
+    res = run('mkdir -p {}{}/'.format(path, file_noext))
+    if res.failed:
+        return False
+    res = run('tar -xzf /tmp/{} -C {}{}/'.format(file_name, path, file_noext))
+    if res.failed:
+        return False
+    res = run('rm /tmp/{}'.format(file_name))
+    if res.failed:
+        return False
+    res = run('mv {0}{1}/web_static/* {0}{1}/'.format(path, file_noext))
+    if res.failed:
+        return False
+    res = run('rm -rf {}{}/web_static'.format(path, file_noext))
+    if res.failed:
+        return False
+    res = run('rm -rf /data/web_static/current')
+    if r.failed:
+        return False
+    res = run('ln -s {}{}/ /data/web_static/current'.format(path, file_noext))
+    if res.failed:
+        return False
+    return True
+    
+def deploy():
+    """Creates and distributes an archive to a web server"""
+    filepath = do_pack()
+    if filepath is None:
+        return False
+    d = do_deploy(filepath)
+    return d 
