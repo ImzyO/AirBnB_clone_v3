ffc37bcb0bd05ade0378d04f401a493e969bee8a
root <100032916+ImzyO@users.noreply.github.com>
2022-10-19 10:22:34 -0700

    added imports

--- models/base_model.py
+++ models/base_model.py
@@ -16,20 +16,20 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-    if kwargs:
-        for key, value in kwargs.items():
-            if key == "created_at" or key == "updated_at":
-                value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-            if key != "__class__":
-                setattr(self, key, value)
-        if "id" not in kwargs.keys():
-            setattr(self, "id", str(uuid.uuid4()))
-        time = datetime.now()
-        if "created_at" not in kwargs.keys():
-            setattr(self, "created_at", time)
-        if "updated_at" not in kwargs.keys():
-            setattr(self, "updated_at", time)
-    else:
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
+            if "id" not in kwargs.keys():
+                setattr(self, "id", str(uuid.uuid4()))
+            time = datetime.now()
+            if "created_at" not in kwargs.keys():
+                setattr(self, "created_at", time)
+            if "updated_at" not in kwargs.keys():
+                setattr(self, "updated_at", time)
+        else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
--- models/state.py
+++ models/state.py
@@ -2,6 +2,9 @@
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import String, Column
+from os import getenv
+from sqlalchemy.orm import relationship
+from models.city import City
 
 class State(BaseModel, Base):
     """ State class """
@@ -9,8 +12,8 @@ class State(BaseModel, Base):
     name = Column(String(128), nullable=False)
 
     if getenv('HBNB_TYPE_STORAGE') == 'db':
-        cities = relationship('City', backref='state', cascade='all, delete-orphan')
+        cities = relationship('City', backref='state', cascade='all, delete=orphan')
     else:
         @property
         def cities(self):
-            return [city for city in models.storage.all(City).values() if city.state_id == self.id
+            return [city for city in models.storage.all(City).values() if city.state_id == self.id]
