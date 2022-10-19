ddf6b5a2513873f7d425cc02a17914fe6c9a238f
Imani <100032916+ImzyO@users.noreply.github.com>
2022-10-19 16:27:37 +0300

    debug

--- console.py
+++ console.py
@@ -2,14 +2,18 @@
 """ Console Module """
 import cmd
 import sys
+import models
+from models import storage
 from models.base_model import BaseModel
-from models.__init__ import storage
 from models.user import User
 from models.place import Place
 from models.state import State
 from models.city import City
 from models.amenity import Amenity
 from models.review import Review
+from datetime import datetime
+from shlex import split
+
 
 
 class HBNBCommand(cmd.Cmd):
